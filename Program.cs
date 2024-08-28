3f5ceccb119201f48313a625a66f13bca1f75a7c
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-28 18:39:01 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DisplayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your String: ");
+            string myString = Console.ReadLine();
+            Console.WriteLine("Enter your Char: ");
+            char x = Char.Parse(Console.ReadLine());
+            Console.WriteLine(x+ " appears "+charCount(myString, x)+" times");
+        }
+
+        public static int charCount(string newString, char x)
+        {
+            int count = 0;
+            for (int i = 0; i < newString.Length; i++)
+            {
+                if (newString[i] == x)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
+
